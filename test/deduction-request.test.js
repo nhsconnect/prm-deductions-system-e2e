22e13af3589311d92bfb71448bbba2cfa33a7a2d
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-11-10 12:23:55 +0000

    [GreenPipeline] Updated devDependencies to latest versions

--- test/deduction-request.test.js
+++ test/deduction-request.test.js
@@ -54,9 +54,8 @@ describe('Deduction request', () => {
 
         // Action
         const deductionRequestResource = await makeDeductionRequest(nhsNumber);
-        const conversationId = extractConversationIdFromDeductionRequestResource(
-          deductionRequestResource
-        );
+        const conversationId =
+          extractConversationIdFromDeductionRequestResource(deductionRequestResource);
         const messageId = v4();
         const attachmentId = v4();
         console.log(`Triggered deduction request, ConversationID: ${conversationId}`);
@@ -141,9 +140,8 @@ describe('Deduction request', () => {
 
         // Action
         const deductionRequestResource = await makeDeductionRequest(nhsNumber);
-        const conversationId = extractConversationIdFromDeductionRequestResource(
-          deductionRequestResource
-        );
+        const conversationId =
+          extractConversationIdFromDeductionRequestResource(deductionRequestResource);
         console.log(`Triggered deduction request, ConversationID: ${conversationId}`);
 
         const expectedStatus = 'ehr_extract_received';
@@ -188,9 +186,8 @@ describe('Deduction request', () => {
 
         // Action
         const deductionRequestResource = await makeDeductionRequest(nhsNumber);
-        const conversationId = extractConversationIdFromDeductionRequestResource(
-          deductionRequestResource
-        );
+        const conversationId =
+          extractConversationIdFromDeductionRequestResource(deductionRequestResource);
         console.log('DeductionRequest conversationId:', conversationId);
 
         const expectedStatus = 'continue_message_sent';
